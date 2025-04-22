298fe4e8c7766a1538b9c8198b7d39093cefc6ad
灿坤 <30515589+zhangckcup@users.noreply.github.com>
2025-04-22 23:50:12 +0800

    add package scripts&&fix SSE/server path

--- SSE/server.js
+++ SSE/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/events', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
